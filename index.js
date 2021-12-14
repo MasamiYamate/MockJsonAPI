d684b7961d03014e0139709453d31495c84bebf3
Masami Yamate <yamate.inquiry@mail.yamatte.net>
2021-12-14 23:14:10 +0900

    status code対応

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const rx = require('rxjs');
 const bodyParser = require('body-parser');
 const requestParser = require('./lib/request-parser');
 const fileAccess = require('./lib/file-access-manager');
 const app = express();
 
-let dataDirectory = process.cwd() + "/data";
+const dataDirectory = process.cwd() + "/data";
 
 // Expressの初期設定
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,8 +20,30 @@ app.use('/*', function(request, response, next) {
     let observer = requestParser.requestJsonPath(dataDirectory, request);
     let result = fileAccess.responseData(observer);
     result.subscribe(result => {
-        response.status(200).json(result);
+        successResponse(result, response);
     }, error => {
-        response.status(404).send(error.message);
+        errorResponse(error, response);
     });
 });
+
+function successResponse(result, response) {
+    const statusCode = result.statusCode ?? null;
+    const responseBody = result.body ?? null;
+    if (statusCode == null) {
+        const message = "Not found status code";
+        const error = new Error(message);
+        errorResponse(error, response);
+    } else if (responseBody == null) {
+        const message = "Not found response body";
+        const error = new Error(message);
+        errorResponse(error, response);
+    } else {
+        response.status(statusCode).json(responseBody);
+    }
+}
+
+function errorResponse(error, response) {
+    const errorCode = 404;
+    const errorMessage = error.message ?? "Unknown error";
+    response.status(errorCode).json(errorMessage);
+}
